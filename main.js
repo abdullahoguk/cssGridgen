84ce64fd040379fa9d6c8630a84388fd392a3ba9
Abdullah <abdullahoguk@gmail.com>
2019-09-19 15:56:35 +0300

    mouse events for selecting grid cells
    Signed-off-by: Abdullah <abdullahoguk@gmail.com>

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var gridData = {
     gridGap:".3em"
 }
 
+
+
 const gridContainer = document.querySelector(".gridContainer");
 const gridWireframe = gridContainer.querySelector(".gridWireframe");
 const rowInput = document.querySelector("input#row");
@@ -14,19 +16,25 @@ createGrid(gridData.numOfRows, gridData.numOfColumns );
 rowInput.addEventListener("change", addRowCol);
 columnInput.addEventListener("change", addRowCol);
 
+var eventState = "none";//none selectStart selectEnd 
+var firstSelectedCell="";
+var lastSelectedCell="";
 
 function createGrid(row,col){
     for(let i=0;i<row;i++){
         for(let j=0;j<col;j++){
             let cell = document.createElement("div");
             let cellNo = i*row+j+1;
-            cell.className = "cell";
+            cell.className = "cell empty";
             cell.style.gridArea=`${i+1}\/${j+1}\/${i+2}\/${j+2}`;
             cell.dataset.no=cellNo;
+            cell.dataset.row=i+1;
+            cell.dataset.column=j+1;
             cell.innerHTML=`<span>${cellNo}</span>`;
 
             gridWireframe.appendChild(cell);
             cell.addEventListener("click",selectCell);
+            cell.addEventListener("mouseover",cellHover)
 
         }     
     }
@@ -51,16 +59,73 @@ function addRowCol(e){
     else{
         console.error("Error when adding row o column")
     }
-    console.log(e)
+    
     gridWireframe.innerHTML="";
     createGrid(gridData.numOfRows, gridData.numOfColumns );
 }
 
 function selectCell(e){
-    e.target.classList.toggle("selected");
-    window.addEventListener("mousemove", mouseMoveNextSelect)
+    if(eventState=="none"){
+        calculateSelection();
+        e.target.classList.add("selected");
+        e.target.classList.add("firstCell");
+        
+        eventState="selectStart";
+        firstSelectedCell=e.target;
+    }
+
+    else if(eventState=="selectStart" && !(e.target == firstSelectedCell)){  
+        eventState="selectEnd";
+        e.target.classList.add("lastCell");
+        lastSelectedCell = e.target;
+        let [firstRow,firstColumn] = [firstSelectedCell.dataset.row, firstSelectedCell.dataset.column];
+        let [lastRow, lastColumn] = [lastSelectedCell.dataset.row, lastSelectedCell.dataset.column];
+        calculateSelection(firstRow,firstColumn,lastRow,lastColumn);
+        eventState="none";
+        firstSelectedCell="";
+        lastSelectedCell="";
+    }
+
+    // cancel selection when clicked on first selected cell
+    else if(eventState=="selectStart"&&e.target == firstSelectedCell){
+        if(e.target.classList.contains("firstCell")){
+            eventState="none";
+            e.target.classList.remove("selected");
+            e.target.classList.remove("firstCell");
+            e.target.classList.remove("selectable");
+        }
+    }
 }
 
-function mouseMoveNextSelect(e){
-    
+function cellHover(e){
+    if(eventState=="selectStart"&&e.target.classList.contains("cell")){
+        let [firstRow,firstColumn] = [firstSelectedCell.dataset.row, firstSelectedCell.dataset.column];
+        let [lastRow, lastColumn] = [e.target.dataset.row, e.target.dataset.column];
+        calculateSelection(firstRow,firstColumn,lastRow,lastColumn);
+    }
+}
+
+function calculateSelection(startX,startY,endX,endY){
+    var [x1, x2] = endX > startX ? [startX,endX]:[endX,startX];
+    var [y1, y2] = endY > startY ? [startY,endY]:[endY,startY];
+    var cells = [...document.querySelectorAll(".cell")];
+    cells.forEach(function(cell){
+        let [cX,cY] = [cell.dataset.row, cell.dataset.column];
+        let coordinantCond = cX<=x2&&cX>=x1 && cY<=y2&&cY>=y1;
+        if(coordinantCond && eventState=="selectStart"){
+           cell.classList.add("selectable");
+           cell.classList.remove("selected");
+        }
+        else if(coordinantCond && eventState=="selectEnd"){
+            cell.classList.add("selected");
+            cell.classList.remove("selectable");
+            cell.classList.remove("firstCell");
+            cell.classList.remove("lastCell");
+        }
+
+        else{
+            cell.classList.remove("selectable");
+            cell.classList.remove("selected");
+        }
+    })
 }
